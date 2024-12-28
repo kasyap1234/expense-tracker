2c79f965a190f7352dfee4d55bbc4c21853d5b6f
Kasyap Dharanikota <kasyap3103@gmail.com>
2024-12-28 15:25:53 +0530

    add main.go

--- main.go
+++ main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"github.com/go-chi/chi"
+	chiMid "github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"github.com/kasyap1234/expense-tracker/config"
+	"github.com/kasyap1234/expense-tracker/routes"
+	appMid "github.com/kasyap1234/expense-tracker/middleware"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	config.InitDB(os.Getenv("DB_URL"))
+
+	r := chi.NewRouter()
+
+	r.Use(chiMid.Logger)
+	r.Use(chiMid.Recoverer)
+	r.Get("/auth/google/login", routes.GoogleLogin)
+	r.Get("/auth/google/callback", routes.OauthCallback)
+
+	// Protected routes
+	r.Route("/expenses", func(r chi.Router) {
+	 r.Use(appMid.JWTMiddleware)
+		r.Post("/", routes.CreateExpense)
+		r.Get("/", routes.GetExpenses)
+		r.Get("/{expenseID}", routes.GetExpense)
+		r.Put("/{expenseID}", routes.UpdateExpense)
+		r.Delete("/{expenseID}", routes.DeleteExpense)
+	})
+
+	http.ListenAndServe(":8080", r)
+
+	http.ListenAndServe(":"+port, r)
+}
--- routes/auth.go
+++ routes/auth.go
@@ -5,15 +5,14 @@ import (
 	"net/http"
 	"os"
 	"time"
-
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/kasy1234//expense-tracker/models"
+	"golang.org/x/oauth2"
 	"github.com/kasyap1234/expense-tracker/config"
 	"github.com/kasyap1234/expense-tracker/models"
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
-// google ouath2 configuration 
+
+// google ouath2 configuration
 var oauthConfig = &oauth2.Config{
 	ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 	ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
@@ -21,17 +20,19 @@ var oauthConfig = &oauth2.Config{
 	Scopes:       []string{"openid", "email", "profile"},
 	Endpoint:     google.Endpoint,
 }
-// gogole login handler : redirection 
+
+// gogole login handler : redirection
 func GoogleLogin(w http.ResponseWriter, r *http.Request) {
 	url := oauthConfig.AuthCodeURL("state")
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 
 }
-// google oauth2 callback handler : gets code and exchanges for token ; 
+
+// google oauth2 callback handler : gets code and exchanges for token ;
 func OauthCallback(w http.ResponseWriter, r *http.Request) {
-	// get code from url query parameters 
+	// get code from url query parameters
 	code := r.URL.Query().Get("code")
-	// exchange code for token ; 
+	// exchange code for token ;
 	token, err := oauthConfig.Exchange(r.Context(), code)
 	if err != nil {
 		http.Error(w, "Failed to exchange token", http.StatusInternalServerError)
@@ -54,12 +55,12 @@ func OauthCallback(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(resp.Body).Decode(&userInfo)
 	var user models.User
 
-	// check if user already exists in database if not create the new user or update it 
+	// check if user already exists in database if not create the new user or update it
 	config.DB.FirstOrCreate(&user, models.User{
 		Email:    userInfo.Email,
 		Username: userInfo.Name,
 	})
-	// generate jwt token and set it as cookie for persistence ; 
+	// generate jwt token and set it as cookie for persistence ;
 	tokenString, err := generateToken(user.ID)
 	if err != nil {
 		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
