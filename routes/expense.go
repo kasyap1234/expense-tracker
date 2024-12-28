38c355985a08a4c18079f3f612829ec5eaba4891
Kasyap Dharanikota <kasyap3103@gmail.com>
2024-12-28 15:25:53 +0530

    add main.go

--- routes/expense.go
+++ routes/expense.go
@@ -1,46 +1,41 @@
 package routes
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
 	"strconv"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/kasyap1234/expense-tracker/config"
 	"github.com/kasyap1234/expense-tracker/models"
 )
 
-
-
-func CreateExpense(w http.ResponseWriter, r *http.Request){
+func CreateExpense(w http.ResponseWriter, r *http.Request) {
 	userID := r.Context().Value("userID").(uint)
 
 	var expense models.Expense
 	json.NewDecoder(r.Body).Decode(&expense)
-	expense.UserID=userID ; 
+	expense.UserID = userID
 	config.DB.Create(&expense)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(expense)
 
 }
 
-func GetExpenses(w http.ResponseWriter, r* http.Request){
-	userID :=r.Context().Value("userID").(uint)
-	var expenses []models.Expense 
-	config.DB.Where("user_id = ?",userID).Find(&expenses)
+func GetExpenses(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value("userID").(uint)
+	var expenses []models.Expense
+	config.DB.Where("user_id = ?", userID).Find(&expenses)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(expenses)
 
 }
 
-func GetExpense(w http.ResponseWriter, r *http.Request){
-	userID :=r.Context().Value("userID").(uint)
+func GetExpense(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value("userID").(uint)
 	var expense models.Expense
-	expenseID :=expense.ID
-	
+	expenseID := expense.ID
 
-	config.DB.Where("id= ?  AND user_id = ?",expenseID,userID).First(&expense)
+	config.DB.Where("id= ?  AND user_id = ?", expenseID, userID).First(&expense)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(expense)
 
@@ -57,3 +52,11 @@ func UpdateExpense(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(expense)
 }
 
+func DeleteExpense(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value("userID").(uint)
+	expenseID, _ := strconv.Atoi(r.URL.Query().Get("ID"))
+	var expense models.Expense
+	config.DB.Where("id= ? AND user_id = ?", expenseID, userID).Delete(&expense)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("Expense deleted successfully ")
+}
